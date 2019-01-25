75f61179bed073366604e099dd675e6393945b45
jialijin0@gmail.com <jialijin0@gmail.com>
2019-01-25 01:01:31 -0400

    rename file name and change directory

--- CSCI2170/A5/js/myScript.js
+++
@@ -1,5 +0,0 @@
-$('#chatButton').on('show.bs.modal', function (event) {
-    var button = $(event.relatedTarget) // Button that triggered the modal
-    var modal = $(this)
-    modal.find('.modal-body input')//.val(chat)
-})
--- mailtome.php
+++
@@ -1,6 +0,0 @@
-<?php
-$fullname = $_POST['name'];
-$email = $_POST['email'];
-$message = $_POST['msg'];
-mail($email,$fullname,$message);
-?>
